0041bf2f2ea2f2afcc848ec9cf646a0837d5f6c5
Gayathri Chinda <91979133+Gayathri5Chinda@users.noreply.github.com>
2024-03-12 13:46:02 +0530

    Create test_Polymorphism.java

---
+++ test_Polymorphism.java
@@ -0,0 +1,30 @@
+//Polymorphism
+
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class test_Polymorphism {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        animal.makeSound();
+        dog.makeSound();
+        cat.makeSound();
+    }
+}
